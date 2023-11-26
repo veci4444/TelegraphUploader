33033b5834daf6fe96fcb7c38cb965e265650c86
veci4444 <124430028+veci4444@users.noreply.github.com>
2023-11-26 18:08:11 +0300

    bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ else:
 async def check_user(id):
     ok = True
     try:
-        await BotzHub(GetParticipantRequest(channel='@NexaBotsUpdates', user_id=id))
+        await BotzHub(GetParticipantRequest(channel='@frobid', user_id=id))
         ok = True
     except UserNotParticipantError:
         ok = False
